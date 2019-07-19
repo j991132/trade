e53e9bbbbb0330bb9220ffba1a35e1a28376f2e9
j991132 <j991132@naver.com>
2019-07-19 14:45:17 +0900

    0719 나라별자원 목표자원 등록 & 나라상태창 글자 안보임 수정중

--- app/src/main/java/com/social/trade/nation.java
+++ app/src/main/java/com/social/trade/nation.java
@@ -22,7 +22,7 @@ public class nation extends AppCompatActivity {
 
     private TextView name;
     private ImageView nationmark;
-    private TextView nowlv, nowoil, nowfe, nowgold, nowwood, nowman, nowmoney;
+    private TextView nowlv, nowoil, nowfe, nowgold, nowwood, nowman, nowmoney, goil, gfe, ggold, gwood, gman;
     private FirebaseFirestore db;
     private String TAG = "activity_nation";
 
@@ -44,37 +44,49 @@ public class nation extends AppCompatActivity {
         nowwood = (TextView) findViewById(R.id.wood);
         nowman = (TextView) findViewById(R.id.man);
         nowmoney = (TextView) findViewById(R.id.money);
+        goil = (TextView) findViewById(R.id.goil);
+        gfe = (TextView) findViewById(R.id.gfe);
+        ggold = (TextView) findViewById(R.id.ggold);
+        gwood = (TextView) findViewById(R.id.gwood);
+        gman = (TextView) findViewById(R.id.gman);
+
 
         switch (nationname){
             case "대한민국":
                 nationmark.setImageResource(R.drawable.kor);
                 name.setText(nationname);
                 getsource(nationname);
+                goalsource(nationname);
                 break;
             case "중국":
                 nationmark.setImageResource(R.drawable.cha);
                 name.setText(nationname);
                 getsource(nationname);
+                goalsource(nationname);
                 break;
             case "호주":
                 nationmark.setImageResource(R.drawable.os);
                 name.setText(nationname);
                 getsource(nationname);
+                goalsource(nationname);
                 break;
             case "캐나다":
                 nationmark.setImageResource(R.drawable.ca);
                 name.setText(nationname);
                 getsource(nationname);
+                goalsource(nationname);
                 break;
             case "사우디아라비아":
                 nationmark.setImageResource(R.drawable.saudi);
                 name.setText(nationname);
                 getsource(nationname);
+                goalsource(nationname);
                 break;
             case "남아프리카공화국":
                 nationmark.setImageResource(R.drawable.sa);
                 name.setText(nationname);
                 getsource(nationname);
+                goalsource(nationname);
                 break;
         }
     }
@@ -105,6 +117,94 @@ public class nation extends AppCompatActivity {
                             nowwood.setText(""+nationwood);
                             nowman.setText(""+nationman);
                             nowmoney.setText(""+nationmoney);
+
+
+
+
+
+                        }else{
+                            Log.d(TAG, "가져오기 실패", task.getException());
+                            //   Toast.makeText(getApplication(), "이미 선택된 나라입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void goalsource(String nationname){
+        //파이어스토어에서 자료 가져오기
+        db = FirebaseFirestore.getInstance();
+        db.collection("나라선택여부").document(nationname)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            Object nationlv = document.getData().get("lv").toString() ;
+                            String nlv = (String) nationlv;
+                            switch (nlv) {
+                                case "0" :
+                                    goil.setText("0");
+                                    gfe.setText("0");
+                                    ggold.setText("0");
+                                    gwood.setText("6");
+                                    gman.setText("6");
+                                 break;
+
+                                case "1" :
+                                    goil.setText("4");
+                                    gfe.setText("0");
+                                    ggold.setText("0");
+                                    gwood.setText("6");
+                                    gman.setText("14");
+                                    break;
+                                case "2" :
+                                    goil.setText("11");
+                                    gfe.setText("0");
+                                    ggold.setText("1");
+                                    gwood.setText("7");
+                                    gman.setText("17");
+                                    break;
+                                case "3" :
+                                    goil.setText("11");
+                                    gfe.setText("8");
+                                    ggold.setText("2");
+                                    gwood.setText("7");
+                                    gman.setText("20");
+                                    break;
+                                case "4" :
+                                    goil.setText("13");
+                                    gfe.setText("13");
+                                    ggold.setText("4");
+                                    gwood.setText("7");
+                                    gman.setText("23");
+                                    break;
+                                case "5" :
+                                    goil.setText("14");
+                                    gfe.setText("17");
+                                    ggold.setText("9");
+                                    gwood.setText("7");
+                                    gman.setText("25");
+                                    break;
+                                case "6" :
+                                    goil.setText("15");
+                                    gfe.setText("20");
+                                    ggold.setText("15");
+                                    gwood.setText("7");
+                                    gman.setText("27");
+                                    break;
+                                case "7" :
+                                    goil.setText("18");
+                                    gfe.setText("25");
+                                    ggold.setText("17");
+                                    gwood.setText("9");
+                                    gman.setText("30");
+                                    break;
+                            }
+
+
+
+
 
 
 
